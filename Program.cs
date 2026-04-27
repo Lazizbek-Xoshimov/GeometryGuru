3130250c198a7ef07e13f47054834532593fea50
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-04-27 21:15:59 +0500

    Dastur code qismi yozildi

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Birinchi sonni kiriting: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ikkinchi sonni kiriting (0 ga teng bo'lmagan): ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Bu sonlar ustida quyidagi 4 ta amal bajarildi:");
+
+Console.WriteLine(a + " + " + b + " = " + (a + b));
+Console.WriteLine(a + " * " + b + " = " + (a * b));
+Console.WriteLine(a + " - " + b + " = " + (a - b));
+Console.WriteLine(a + " / " + b + " = " + (Convert.ToDecimal(a) / Convert.ToDecimal(b)));
