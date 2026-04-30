9979df2e3c70528b667cce7460a63c2c26a01b8f
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-01 00:41:56 +0500

    switch case tanlov operatori orqali modified qilindi

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
-﻿Console.Write("Birinchi sonni kiriting: ");
+﻿Console.WriteLine("Qaysi amal ustida ishlamoqchisiz?");
+Console.WriteLine("- Qo'shish (+)");
+Console.WriteLine("- Ko'paytirish (*)");
+Console.WriteLine("- Ayirish (-)");
+Console.WriteLine("- Bo'lish (/)");
+Console.WriteLine("- Qoldiqni hisoblash (%)");
+
+Console.Write("Kerakli arifmetik operatorni kiriting: ");
+string str = Console.ReadLine();
+char departmentCharacter = Convert.ToChar(str);
+
+Console.Write("Birinchi sonni kiriting: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Ikkinchi sonni kiriting (0 ga teng bo'lmagan): ");
+Console.Write("Ikkinchi sonni kiriting: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Bu sonlar ustida quyidagi 4 ta amal bajarildi:");
-
-Console.WriteLine(a + " + " + b + " = " + (a + b));
-Console.WriteLine(a + " * " + b + " = " + (a * b));
-Console.WriteLine(a + " - " + b + " = " + (a - b));
-Console.WriteLine(a + " / " + b + " = " + (Convert.ToDecimal(a) / Convert.ToDecimal(b)));
+switch (departmentCharacter)
+{
+    case '+':
+        Console.WriteLine(a + " + " + b + " = " + (a + b));
+        break;
+    case '*':
+        Console.WriteLine(a + " * " + b + " = " + (a * b));
+        break;
+    case '-':
+        Console.WriteLine(a + " - " + b + " = " + (a - b));
+        break;
+    case '/':
+        {
+            if(b != 0)
+                Console.WriteLine(a + " / " + b + " = " + (a / b));
+            else
+                Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
+            break;
+        }
+    case '%':
+        if(b != 0)
+                Console.WriteLine(a + " % " + b + " = " + (a % b));
+            else
+                Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
+            break;
+    default:
+        Console.WriteLine("Boshqa belgi kiritdingiz!\nDasturni qaytadan ishga tushiring.");
+        break;
+}
