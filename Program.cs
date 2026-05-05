595cb887465d4135e4786cd71e255a47f083677d
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-05 23:37:51 +0500

    Dasturdan chiqish yoki qolish bo'limi qo'shildi

--- Program.cs
+++ Program.cs
@@ -1,73 +1,84 @@
 ﻿using System.Drawing;
 
-Console.WriteLine("1. Sonning darajasini topish");
-Console.WriteLine("2. [1; n] intervalda tub sonlar va ularning yig'indisini hisoblash");
-Console.WriteLine("3. Arifmetik amallarni hisoblash");
-Console.Write("Kerakli bo'limni tanlang: ");
+string doQuit = string.Empty;
 
-string option = Console.ReadLine();
-
-switch (option)
+do
 {
-    case "1":
-        {
-            break;
-        }
-    case "2":
-        {
-            break;
-        }
-    case "3":
-        {
-            Console.WriteLine("Kerakli hisoblash ketma-ketligini kiriting: (a + b =)");
-            string calculate = Console.ReadLine();
+    Console.WriteLine("1. Sonning darajasini topish");
+    Console.WriteLine("2. [1; n] intervalda tub sonlar va ularning yig'indisini hisoblash");
+    Console.WriteLine("3. Arifmetik amallarni hisoblash");
+    Console.Write("Kerakli bo'limni tanlang: ");
 
-            string[] operators = calculate.Trim().Split();
+    string option = Console.ReadLine();
 
-            char departmentCharacter = Convert.ToChar(operators[1]);
-            int a = Convert.ToInt32(operators[0]);
-            int b = Convert.ToInt32(operators[2]);
-
-            Console.Clear();
-            switch (departmentCharacter)
+    switch (option)
+    {
+        case "1":
             {
-                case '+':
-                    Console.WriteLine(a + " + " + b + " = " + (a + b));
-                    break;
-                case '*':
-                    Console.WriteLine(a + " * " + b + " = " + (a * b));
-                    break;
-                case '-':
-                    Console.WriteLine(a + " - " + b + " = " + (a - b));
-                    break;
-                case '/':
-                    {
-                        if(b != 0)
-                            Console.WriteLine(a + " / " + b + " = " + (a / b));
-                        else
-                            {
-                                Console.BackgroundColor = ConsoleColor.Red;
-                                Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
-                                Console.ResetColor();
-                            }
-                        break;
-                    }
-                case '%':
-                    {
-                        if(b != 0)
-                            Console.WriteLine(a + " % " + b + " = " + (a % b));
-                        else
-                            {
-                                Console.BackgroundColor = ConsoleColor.Red;
-                                Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
-                                Console.ResetColor();
-                            }
-                        break;   
-                    }
-                default:
-                    Console.WriteLine("Boshqa belgi kiritdingiz!\nDasturni qaytadan ishga tushiring.");
-                    break;
+                break;
             }
-            break;
-        }
-}
+        case "2":
+            {
+                break;
+            }
+        case "3":
+            {
+                Console.WriteLine("Kerakli hisoblash ketma-ketligini kiriting: (a + b =)");
+                string calculate = Console.ReadLine();
+
+                string[] operators = calculate.Trim().Split();
+
+                char departmentCharacter = Convert.ToChar(operators[1]);
+                int a = Convert.ToInt32(operators[0]);
+                int b = Convert.ToInt32(operators[2]);
+
+                Console.Clear();
+                switch (departmentCharacter)
+                {
+                    case '+':
+                        Console.WriteLine(a + " + " + b + " = " + (a + b));
+                        break;
+                    case '*':
+                        Console.WriteLine(a + " * " + b + " = " + (a * b));
+                        break;
+                    case '-':
+                        Console.WriteLine(a + " - " + b + " = " + (a - b));
+                        break;
+                    case '/':
+                        {
+                            if(b != 0)
+                                Console.WriteLine(a + " / " + b + " = " + (a / b));
+                            else
+                                {
+                                    Console.BackgroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
+                                    Console.ResetColor();
+                                }
+                            break;
+                        }
+                    case '%':
+                        {
+                            if(b != 0)
+                                Console.WriteLine(a + " % " + b + " = " + (a % b));
+                            else
+                                {
+                                    Console.BackgroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
+                                    Console.ResetColor();
+                                }
+                            break;   
+                        }
+                    default:
+                        Console.WriteLine("Boshqa belgi kiritdingiz!\nDasturni qaytadan ishga tushiring.");
+                        break;
+                }
+                break;
+            }
+    }
+
+    Console.WriteLine("Dasturdan chiqishni xohlaysizmi?");
+    Console.Write("(ha/yo'q): ");
+    doQuit = Console.ReadLine();
+
+    Console.Clear();
+} while (doQuit.ToLower() == "yo'q");
