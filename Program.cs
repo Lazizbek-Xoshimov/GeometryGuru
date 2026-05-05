7d5eb993fefa4a4759c7d65d12fc9523f49cbc8e
Lazizbek-Xoshimov <lazizbekaxion19@gmail.com>
2026-05-05 23:21:11 +0500

    Daraja va n gacha tub sonlar yig'indisi hisoblashni qo'shish uchun menu yaratildi

--- Program.cs
+++ Program.cs
@@ -1,51 +1,73 @@
 ﻿using System.Drawing;
 
-Console.WriteLine("Kerakli hisoblash ketma-ketligini kiriting: (a + b =)");
-string calculate = Console.ReadLine();
+Console.WriteLine("1. Sonning darajasini topish");
+Console.WriteLine("2. [1; n] intervalda tub sonlar va ularning yig'indisini hisoblash");
+Console.WriteLine("3. Arifmetik amallarni hisoblash");
+Console.Write("Kerakli bo'limni tanlang: ");
 
-string[] operators = calculate.Trim().Split();
+string option = Console.ReadLine();
 
-char departmentCharacter = Convert.ToChar(operators[1]);
-int a = Convert.ToInt32(operators[0]);
-int b = Convert.ToInt32(operators[2]);
-
-Console.Clear();
-switch (departmentCharacter)
+switch (option)
 {
-    case '+':
-        Console.WriteLine(a + " + " + b + " = " + (a + b));
-        break;
-    case '*':
-        Console.WriteLine(a + " * " + b + " = " + (a * b));
-        break;
-    case '-':
-        Console.WriteLine(a + " - " + b + " = " + (a - b));
-        break;
-    case '/':
+    case "1":
         {
-            if(b != 0)
-                Console.WriteLine(a + " / " + b + " = " + (a / b));
-            else
-                {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
-                    Console.ResetColor();
-                }
             break;
         }
-    case '%':
+    case "2":
         {
-            if(b != 0)
-                Console.WriteLine(a + " % " + b + " = " + (a % b));
-            else
-                {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
-                    Console.ResetColor();
-                }
-            break;   
+            break;
         }
-    default:
-        Console.WriteLine("Boshqa belgi kiritdingiz!\nDasturni qaytadan ishga tushiring.");
-        break;
+    case "3":
+        {
+            Console.WriteLine("Kerakli hisoblash ketma-ketligini kiriting: (a + b =)");
+            string calculate = Console.ReadLine();
+
+            string[] operators = calculate.Trim().Split();
+
+            char departmentCharacter = Convert.ToChar(operators[1]);
+            int a = Convert.ToInt32(operators[0]);
+            int b = Convert.ToInt32(operators[2]);
+
+            Console.Clear();
+            switch (departmentCharacter)
+            {
+                case '+':
+                    Console.WriteLine(a + " + " + b + " = " + (a + b));
+                    break;
+                case '*':
+                    Console.WriteLine(a + " * " + b + " = " + (a * b));
+                    break;
+                case '-':
+                    Console.WriteLine(a + " - " + b + " = " + (a - b));
+                    break;
+                case '/':
+                    {
+                        if(b != 0)
+                            Console.WriteLine(a + " / " + b + " = " + (a / b));
+                        else
+                            {
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
+                                Console.ResetColor();
+                            }
+                        break;
+                    }
+                case '%':
+                    {
+                        if(b != 0)
+                            Console.WriteLine(a + " % " + b + " = " + (a % b));
+                        else
+                            {
+                                Console.BackgroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Ikkinchi son 0 bo'lishi mumkin emas!");
+                                Console.ResetColor();
+                            }
+                        break;   
+                    }
+                default:
+                    Console.WriteLine("Boshqa belgi kiritdingiz!\nDasturni qaytadan ishga tushiring.");
+                    break;
+            }
+            break;
+        }
 }
